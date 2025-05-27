9f02fe3c027b336247bf3b16b747310c7def7d84
mr-rt95 <mr.rtahir95@gmail.com>
2025-05-27 12:44:34 +0400

    feat: add initial JavaScript setup for interactive features

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+const burger = document.getElementById('burger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+});
